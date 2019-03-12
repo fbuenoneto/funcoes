3174560238bb911a7ab072e5b48d736a40ca4632
1831127 <1831127@LABBMAQ12.itp.local>
2019-03-12 11:28:25 -0300

    funções

---
+++ exercicio1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Soma</title>
+    </head>
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
