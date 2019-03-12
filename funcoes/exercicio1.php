e6c3cc91a77581a58e9a75e2d9ad3a054a0f9613
1831127 <1831127@LABBMAQ12.itp.local>
2019-03-12 11:43:30 -0300

    funções 1

---
+++ funcoes/exercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Soma</title>
+    </head>
+    <body>
+        
+        <form action="resultado-soma.php" method="POST">
+                Número: <input type="text" name="número1">
+                Número: <input type="text" name="número2">
+                <button type="submit">Somar</botton>
+        </form>
+
+            
+    </body>
+</html>
