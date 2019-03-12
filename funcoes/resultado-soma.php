77408f1f0144691be021b6b39d133cadf73ed0f0
1831127 <1831127@LABBMAQ12.itp.local>
2019-03-12 11:44:44 -0300

    funções 2

---
+++ funcoes/resultado-soma.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $n1= $_POST["número1"];
+            $n2= $_POST["número2"];
+            $soma= $n1+$n2;
+            echo "A soma dos dois números é: ".$soma;
+        ?>
+    </body>
+</html>
